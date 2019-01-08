6428c73a81c1245f93ee5bb8077ea5b4956a8c32
Sodam Baek <bsodam@gmail.com>
2019-01-08 21:09:44 +0900

    [feature] add list page

--- mysite/eats/urls.py
+++ mysite/eats/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'eats'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('list/', views.ListView.as_view(), name='list'),
 ]
--- mysite/eats/views.py
+++ mysite/eats/views.py
@@ -6,3 +6,10 @@ from eats.models import Restaurant
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'eats/index.html'
+
+class ListView(generic.ListView):
+    template_name = 'eats/list.html'
+    context_object_name = 'restaurant_list'
+
+    def get_queryset(self):
+        return Restaurant.objects.order_by('id')

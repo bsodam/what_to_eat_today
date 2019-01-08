6e94388a35242bf2868bf15010167aa0f21371ae
Sodam Baek <bsodam@gmail.com>
2019-01-08 12:46:55 +0900

    initial commit

---
+++ mysite/eats/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Restaurants(models.Model):
+    restaurant_name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    info = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.restaurant_name

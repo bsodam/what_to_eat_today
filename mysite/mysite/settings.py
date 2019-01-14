fc534469753db7d1db1f499f0f0deedd372bbea4
Sodam Baek <bsodam@gmail.com>
2019-01-14 09:55:54 +0000

    [bugfix]settings.py for pythonanywhere

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$uxl!(lil=*@sj4yf5cp4g31wfd&^7gkk#a=od%#umnv8e0mu%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bsodam.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'www_dir', 'static')
